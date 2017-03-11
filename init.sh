a9abf3d1f85a7fccd5c0c5e700d7012bbc82d61b
box <banket@gmail.com>
2017-03-11 03:16:42 -0800

    f-cking semicolon

--- init.sh
+++ init.sh
@@ -1,7 +1,4 @@
-sudo unlink /etc/nginx/sites-enabled/default
+sudo rm /etc/nginx/sites-enabled/test.conf
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
-sudo /etc/init.d/gunicorn restart
-﻿sudo /etc/init.d/mysql start
 
