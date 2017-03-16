1d7437f6e6b8e8a4e77ca81bc58a09844f439b5b
box <banket@gmail.com>
2017-03-16 11:50:39 -0700

    Config changes

--- init.sh
+++ init.sh
@@ -2,6 +2,6 @@ sudo rm /etc/nginx/sites-enabled/test.conf
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
-sudo gunicorn -c /etc/gunicorn.d/hello.py wsgi_application:application
+sudo gunicorn -c /etc/gunicorn.d/hello.py hello:wsgi_application
 sudo /etc/init.d/gunicorn restart
 
