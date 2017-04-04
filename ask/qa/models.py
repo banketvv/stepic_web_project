ca49591d81e4bb76946f378882c80a88f01c9a85
box <banket@gmail.com>
2017-04-04 06:28:11 +0500

    Step 2.5.8 - Django application - fix error #6

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -30,7 +30,7 @@ class Question(models.Model):
 
 class Answer(models.Model):
     text = models.TextField()
-    added_at = models.DateField()
+    added_at = models.DateField(auto_now_add=True)
     question = models.ForeignKey(Question)
     author = models.ForeignKey(User)
 
